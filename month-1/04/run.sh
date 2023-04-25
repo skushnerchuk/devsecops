7b0cf77f3f00b9dd536cfb2dcfca9482087c9d9d
Sergey Kushnerchuk <webmaster@sk-developer.ru>
2023-04-25 12:45:41 +0300

    Update run.sh

--- month-1/04/run.sh
+++ month-1/04/run.sh
@@ -1,5 +1,5 @@
 docker pull ket9/otus-devsecops-owasp-rest
-docker run -d -p 8080:8080 --name otus-05 -e tokentimetolive=6000 ket9/otus-devsecops-owasp-rest:latest
+docker run -d -p 8080:8080 --name otus-05 ket9/otus-devsecops-owasp-rest:latest
 while ! nc -z localhost 8080; do
   sleep 1.0
 done
